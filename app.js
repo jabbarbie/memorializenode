1777ba3c3e386ec9cb0b543d8e8e67fec1ffb02e
Fauzi Jabbar <krotopal@gmail.com>
2024-07-28 01:22:41 +0700

    appserv

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var app = express();
 // Routes
@@ -8,14 +7,3 @@ app.get('/', function (req, res) {
 // Listen
 var port = process.env.PORT || 3000;
 app.listen(port); console.log('Listening on localhost:' + port);
-=======
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
-});
-server.listen();
->>>>>>> 2d7e1be (asd)
