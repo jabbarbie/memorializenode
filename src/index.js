9e36298eedbac31fedd2e827622ee8ba1deb55c2
Fauzi Jabbar <krotopal@gmail.com>
2024-07-28 01:09:38 +0700

    rebase index

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
-<<<<<<< HEAD
-var express = require('express');
-var app = express();
-
-// Routes
-app.get('/', function (req, res) {
-    res.send('maintance, proses migrate dari laravel ke expressjs...');
-});
-
-// Listen
-var port = process.env.PORT || 3000;
-app.listen(port); console.log('Listening on localhost:' + port);
-=======
 var http = require('http');
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
+    var message = 'maintance, proses migrate dari laravel ke expressjs... \n',
         version = 'NodeJS ' + process.versions.node + '\n',
         response = [message, version].join('\n');
     res.end(response);
 });
 server.listen();
->>>>>>> a620f8e (delete index)
